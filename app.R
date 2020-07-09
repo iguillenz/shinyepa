af343520ec07dd84f82c4747e2ae63f7487c6e1b
iguillenz <68084099+iguillenz@users.noreply.github.com>
2020-07-10 00:45:27 +0200

    Update app.R

--- app.R
+++ app.R
@@ -1,10 +1,11 @@
-require(ggplot2)
-require(shiny)
-require(plotly)
-require(wesanderson)
-require(sas7bdat)
-require(haven)
-require( shinydashboard)
-require(forecast)
+
+if(!require(ggplot2)){install.packages("ggplot2")}
+if(!require(shiny)){install.packages("shiny")}
+if(!require(plotly)){install.packages("plotly")}
+if(!require(wesanderson)){install.packages("wesanderson")}
+if(!require(sas7bdat)){install.packages("sas7bdat")}
+if(!require(haven)){install.packages("haven")}
+if(!require(shinydashboard)){install.packages("shinydashboard")}
+if(!require(forecast)){install.packages("forecast")}
 load(datos.Rdata)
 shinyApp(ui = ui,server = server)
